bd3687252addde6081064f899fc265b68ef4f9c4
scambaiterkshitij <158801504+scambaiterkshitij@users.noreply.github.com>
2026-02-20 09:47:32 +0530

    Add banner display function with version info

---
+++ banner.py
@@ -0,0 +1,9 @@
+from config import VERSION
+
+def show_banner():
+    print(f"""
+=============================================
+ Core Defense - Authentication Audit Toolkit
+ Version: {VERSION}
+=============================================
+""")
